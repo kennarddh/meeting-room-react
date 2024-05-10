ce47b5bd2867f8d45b3c2b5be2c456eec1c2eea5
kennarddh <79390992+kennarddh@users.noreply.github.com>
2024-05-10 22:49:13 +0700

    feat: Home styles

--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -1,12 +1,106 @@
 import { FC } from 'react'
 
-import DateTimePicker from 'Components/DateTimePicker/DateTimePicker'
+import { useNavigate } from 'react-router-dom'
+
+import {
+	BookMeetingButton,
+	BookMeetingButtonContainer,
+	Container,
+	IncomingMeeting,
+	IncomingMeetingDatetime,
+	IncomingMeetingText,
+	IncomingMeetingsList,
+	Left,
+	LeftText,
+	LeftTitle,
+	Right,
+	RightTitle,
+} from './Styles'
 
 const Home: FC = () => {
+	const NavigateHook = useNavigate()
+
 	return (
-		<div>
-			<DateTimePicker value={new Date()} />
-		</div>
+		<Container>
+			<Left>
+				<LeftTitle>Available</LeftTitle>
+				<LeftText>
+					Next meeting is starting in 1.5 hours, Organized by{' '}
+					<strong>Sales</strong>
+				</LeftText>
+				<BookMeetingButtonContainer>
+					<BookMeetingButton onClick={() => NavigateHook('/new')}>
+						Book Now
+					</BookMeetingButton>
+				</BookMeetingButtonContainer>
+			</Left>
+			<Right>
+				<RightTitle>Coming Next</RightTitle>
+				<IncomingMeetingsList>
+					<IncomingMeeting>
+						<IncomingMeetingDatetime>
+							7:00 - 12:00 | DD - MM - YYYY
+						</IncomingMeetingDatetime>
+
+						<IncomingMeetingText>
+							Room Occupied by Finance
+						</IncomingMeetingText>
+					</IncomingMeeting>
+					<IncomingMeeting>
+						<IncomingMeetingDatetime>
+							7:00 - 12:00 | DD - MM - YYYY
+						</IncomingMeetingDatetime>
+
+						<IncomingMeetingText>
+							Room Occupied by Finance
+						</IncomingMeetingText>
+					</IncomingMeeting>
+					<IncomingMeeting>
+						<IncomingMeetingDatetime>
+							7:00 - 12:00 | DD - MM - YYYY
+						</IncomingMeetingDatetime>
+
+						<IncomingMeetingText>
+							Room Occupied by Finance
+						</IncomingMeetingText>
+					</IncomingMeeting>
+					<IncomingMeeting>
+						<IncomingMeetingDatetime>
+							7:00 - 12:00 | DD - MM - YYYY
+						</IncomingMeetingDatetime>
+
+						<IncomingMeetingText>
+							Room Occupied by Finance
+						</IncomingMeetingText>
+					</IncomingMeeting>
+					<IncomingMeeting>
+						<IncomingMeetingDatetime>
+							7:00 - 12:00 | DD - MM - YYYY
+						</IncomingMeetingDatetime>
+
+						<IncomingMeetingText>
+							Room Occupied by Finance
+						</IncomingMeetingText>
+					</IncomingMeeting>
+					<IncomingMeeting>
+						<IncomingMeetingDatetime>
+							7:00 - 12:00 | DD - MM - YYYY
+						</IncomingMeetingDatetime>
+						<IncomingMeetingText>
+							Room Occupied by Finance
+						</IncomingMeetingText>
+					</IncomingMeeting>
+					<IncomingMeeting>
+						<IncomingMeetingDatetime>
+							7:00 - 12:00 | DD - MM - YYYY
+						</IncomingMeetingDatetime>
+						<IncomingMeetingText>
+							Room Occupied by Finance
+						</IncomingMeetingText>
+					</IncomingMeeting>
+				</IncomingMeetingsList>
+			</Right>
+		</Container>
 	)
 }
 
---
+++ src/Pages/Home/Styles.tsx
@@ -0,0 +1,134 @@
+import { styled } from 'styled-components'
+
+export const Container = styled.div`
+	width: 100%;
+	height: 100%;
+
+	display: flex;
+	flex-direction: row;
+`
+
+export const Left = styled.div<{ $busy?: boolean }>`
+	width: 60%;
+	height: 100%;
+
+	display: flex;
+	align-items: center;
+	flex-direction: column;
+
+	background-color: ${({ $busy }) => ($busy ? '#ff000099' : 'transparent')};
+`
+
+export const LeftTitle = styled.h2`
+	width: 100%;
+	height: 30%;
+
+	text-align: center;
+
+	color: #ffffff;
+
+	font-size: 100px;
+`
+
+export const LeftText = styled.p`
+	width: 100%;
+	height: 10%;
+
+	text-align: center;
+
+	color: #ffffff;
+
+	font-size: 20px;
+`
+
+export const BookMeetingButtonContainer = styled.div`
+	width: 100%;
+	height: 50%;
+
+	display: flex;
+	justify-content: center;
+	align-items: center;
+`
+
+export const BookMeetingButton = styled.button`
+	width: 50%;
+	height: 50%;
+
+	background-color: transparent;
+
+	font-size: 30px;
+
+	border: 2px solid #ffffff;
+	border-radius: 12px;
+
+	color: white;
+
+	cursor: pointer;
+
+	&:hover {
+		background-color: #458de6;
+	}
+`
+
+export const Right = styled.div`
+	width: 40%;
+	height: 100%;
+
+	display: flex;
+	flex-direction: column;
+`
+
+export const RightTitle = styled.h3`
+	width: 100%;
+	height: 10%;
+
+	padding: 0 20px;
+
+	display: flex;
+	align-items: center;
+	justify-content: flex-end;
+
+	color: #ffffff;
+`
+
+export const IncomingMeetingsList = styled.div`
+	width: 100%;
+	height: 90%;
+
+	display: grid;
+	grid-template-columns: 1fr;
+	grid-auto-rows: minmax(30%, auto);
+	gap: 20px;
+
+	padding-bottom: 20px;
+
+	overflow-y: scroll;
+`
+
+export const IncomingMeeting = styled.div`
+	width: 90%;
+
+	background-color: #458de6;
+
+	border-radius: 12px;
+
+	padding: 10px 20px;
+
+	display: flex;
+	justify-content: space-between;
+	align-items: flex-end;
+	flex-direction: column;
+`
+
+export const IncomingMeetingDatetime = styled.p`
+	color: #ffffff;
+
+	font-size: 2vw;
+`
+
+export const IncomingMeetingText = styled.p`
+	color: #ffffff;
+
+	font-weight: bold;
+	font-size: 2.2vw;
+`
